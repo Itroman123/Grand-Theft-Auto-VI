636e8d5042596a6365bf2d077511a24f5f4c3d43
Itroman123 <143260109+Itroman123@users.noreply.github.com>
2024-04-07 21:21:01 +0300

    rickroll

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var showVideoButton = document.getElementById('showVideoButton');
+  showVideoButton.addEventListener('click', function() {
+    window.location.href = "video.html";
+  });
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var Button = document.getElementById('Button');
+  Button.addEventListener('click', function() {
+    window.location.href = "trailer.html";
+  });
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var showVideo = document.getElementById('showVideo');
+  showVideo.addEventListener('click', function() {
+    window.location.href = "video.html";
+  });
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var description = document.getElementById('description');
+  description.addEventListener('click', function() {
+    window.location.href = "video.html";
+  });
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var praesidium = document.getElementById('praesidium');
+  praesidium.addEventListener('click', function() {
+    window.location.href = "video.html";
+  });
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var communitas = document.getElementById('communitas');
+  communitas.addEventListener('click', function() {
+    window.location.href = "trailer.html";
+  });
+});
+
+
+  
+  
